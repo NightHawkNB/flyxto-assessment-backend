5e6450dab157586dceb4a05efbfce1ed49b5fbbe
NightHawkNB <nipunbathiya1256@outlook.com>
2026-02-10 11:13:39 +0530

    [NEW] Websocket notification

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 import express from 'express';
+import { createServer } from 'http';
 import multer from 'multer';
 import { handleFileUpload } from './fileUploadHandler.js';
 import "dotenv/config";
+
+// Socket modules
+import { Server } from "socket.io";
+import cors from 'cors';
 
 const app = express()
 const port = 4000
 const upload = multer({ storage: multer.memoryStorage() });
 
+const server = createServer(app);
+
+// Socket initialization
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,6 +31,27 @@ app.get('/', (req, res) => {
 })
 
 let tasks = [];
+
+// Socket connection handling
+io.on("connection", (socket) => {
+    console.log("A user connected: " + socket.id);
+
+    // Handling 'task update' event
+    socket.on('task update', (msg) => {
+        console.log('Message Received: ' + msg);
+
+        // socket.broadcast.emit() sends the message to all clients except the sender
+        socket.broadcast.emit('task update', `${socket.id} said: ${msg}`);
+
+        // io.emit() sends the message to all clients including the sender
+        // io.emit('chat message', `${socket.id} said: ${msg}`);
+    });
+
+    // Handling disconection event
+    socket.on("disconnect", () => {
+        console.log("A user disconnected: " + socket.id);
+    })
+});
 
 app.post("/api/uploadthing", upload.single("file"), handleFileUpload);
 
@@ -64,6 +101,6 @@ app.put('/api/tasks/:id', (req, res) => {
     }
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+server.listen(port, () => {
+  console.log(`App listening on port ${port}`)
 })
